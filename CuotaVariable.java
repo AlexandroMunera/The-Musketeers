f504055d96d4c7cad63ac1914f3df1cb88e65f54
Alexandro Múnera <fmunera@rentingcolombia.com>
2018-05-19 16:00:03 -0500

    Delete form menu and add menu bar

--- CuotaVariable.java
+++ CuotaVariable.java
@@ -32,10 +32,24 @@ public class CuotaVariable extends JFrame implements ActionListener
 		super("Cuota Variable - Finanzas Personales - The Musketeers");
 		contenedor = getContentPane();
         contenedor.setLayout(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(700,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//setSize(700,600);
 		setVisible(true);
-		
+
+		JMenu menu, submenu;  
+		JMenuItem menuItemSalir; 
+
+          JMenuBar mb = new JMenuBar();  
+          menu=new JMenu("Operaciones  Ctrl+O");  
+          //submenu=new JMenu("Sub Menu");  
+		  menuItemSalir=new JMenuItem("Salir  Ctrl+x");
+		   
+          menu.add(menuItemSalir);  
+          //menu.add(submenu);  
+          mb.add(menu);  
+          this.setJMenuBar(mb);  
+          
 		ImageIcon img = new ImageIcon("Images/Icono.png");
 		setIconImage(img.getImage());
 		
@@ -49,10 +63,13 @@ public class CuotaVariable extends JFrame implements ActionListener
 		btnCuotaVariable.addActionListener(this);
 		contenedor.add(btnCuotaVariable);
 
-		lblIngreseCapital = new JButton("Ingrese el capital:");
-		btnCuotaFija.setBounds(10,10,100,30);
-		btnCuotaFija.addActionListener(this);
-		contenedor.add(btnCuotaFija);
+		lblIngreseCapital = new JLabel("Ingrese el capital:");
+		lblIngreseCapital.setBounds(10,30,100,30);
+		contenedor.add(lblIngreseCapital);
+
+		txtCapital = new JTextField();
+		txtCapital.setBounds(10,65,100,20);
+		contenedor.add(txtCapital);
 
 				
 		//dibujo el contenedor en pantalla
@@ -78,6 +95,24 @@ public class CuotaVariable extends JFrame implements ActionListener
 			frame.setVisible(true);
 			this.setVisible(false);
 		}
+		
+	}
+
+	public static void main(String args[])
+	{
+		//m�todo principal
+		CuotaVariable p = new CuotaVariable();
+		p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p.setSize(700,600);
+		p.setVisible(true);
+		p.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//p.setUndecorated(true);
+		
+		ImageIcon img = new ImageIcon("Images/Icono.png");
+		p.setIconImage(img.getImage());
+
+		//Image imgBackground = Toolkit.getDefaultToolkit().createImage("Images/background.jpg");
+
 		
 	}
 	//fin del programa
